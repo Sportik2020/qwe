79c96fbb4c69edbeca70cf28f7f96ed1add86557
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-09-29 18:44:11 +0300

    Add files via upload

---
+++ work2.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+int main() {
+/*	int a;
+	if (a > 0) {
+		a = a + 1;//a+=1;a++;a++;
+	}
+	else {
+		a = a - 1;//a-=1;a--;a--a;
+		}
+	*/
+	//1
+	/*	int a;
+		cin >> a;
+		if (a > 0) {
+			 a += 1;
+		}
+		else{
+			a -= 1;
+		}
+		cout << a << endl;
+	*/
+	//2
+	/*	int a;
+		cin >> a;
+		if (a > 0) {
+			 a += 1;
+		}
+		else{
+			a -= 2;
+		}
+		cout << a << endl;
+	*/
+	//3
+	/*	int a;
+		cin >> a;
+		if (a > 0) {
+			 a ++;
+		}
+		else{
+		if (a < 0) {
+			a = a - 2;
+		}
+		else{
+			a = 10;
+		}
+		}
+		cout << a << endl;
+	*/
+	//4
+/*	int a, b, c, d;
+	cin >> a >> b >> c;
+	d = 0;
+	if (a > 0) {
+		d++;
+	}
+
+	if (b > 0) {
+		d++;
+	}
+
+	if (c > 0) {
+		d++;
+	}
+	cout << d << endl;
+*/
+//5
+/*	int a, b, c, d, x;
+	cin >> a >> b >> c;
+	d = 0;
+	x = 0;
+	if (a > 0) {
+		d += 1;
+	}
+	else {
+		x += 1;
+
+	}
+
+	if (b > 0) {
+		d += 1;
+	}
+	else {
+		x += 1;
+	}
+
+	if (c > 0) {
+		d += 1;
+	}
+	else {
+		x += 1;
+	}
+	cout << d << endl;
+	cout << x << endl;
+*/
+//6
+/*	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << a << endl;
+	}
+	else {
+		cout << b << endl;
+	}
+*/
+//7
+/*	int a, b;
+	cin >> a >> b;
+	if (a < b) {
+		cout << a << endl;
+	}
+	else {
+		cout << b << endl;
+	}
+*/
+//8
+/*	int a, b;
+	cin >> a >> b;
+	if (a < b) {
+		cout << b << endl;
+		cout << a << endl;
+	}
+	else {
+		cout << a << endl;
+		cout << b << endl;
+	}
+*/
+//9
+/*	int a, b, c;
+	cin >> a >> b;
+	if (a < b) {
+		cout << a;
+		cout << b;
+	}
+	else{
+		c = a;
+		a = b;
+		b = c;
+		cout << a << endl;
+		cout << b << endl;
+
+	}
+*/
+//10
+/*	int a, b;
+	cin >> a >> b;
+	if (a != b) {
+		a = a + b;
+		b = a + b
+	}
+	else {
+		a = b = 0;
+	}
+	*/
+	return 0;
+}
